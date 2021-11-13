9127d5243983f28b7dfa233c60621b62127bb9ec
Dmitry Larkin <dmitry.larkin@gmail.com>
2021-11-13 22:31:47 +0000

    basic project settings to be senseble

--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("hi there")
+	name := os.Getenv("PWD")
+
+	fmt.Printf("Well done, you are running app in a %s folder\n\n", name)
 }
